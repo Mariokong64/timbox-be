a0210292ba5b4b492bd63a68ff7663622c9c11ec
Mario Kong <as_mario_123@hotmail.com>
2026-08-05 23:55:14 -0600

    eliminación de migrations

--- migrations/20260803_perfil_usuario.sql
+++
@@ -1,9 +0,0 @@
-BEGIN;
-
-ALTER TABLE sys.usuarios
-ADD COLUMN IF NOT EXISTS foto_perfil TEXT;
-
-COMMENT ON COLUMN sys.usuarios.foto_perfil IS
-'Nombre del archivo de la fotografía de perfil almacenada fuera de la base de datos.';
-
-COMMIT;
